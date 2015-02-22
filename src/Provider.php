afe462febc66a80fc103f994fe97cc1203c516fd
DraperStudio <hello@draperstud.io>
2015-02-22 08:24:36 +0100

    first commit

---
+++ src/Provider.php
@@ -0,0 +1,78 @@
+<?php
+namespace SocialiteProviders\Zendesk;
+
+use Laravel\Socialite\Two\AbstractProvider;
+use Laravel\Socialite\Two\ProviderInterface;
+use Laravel\Socialite\Two\User;
+
+class Provider extends AbstractProvider implements ProviderInterface
+{
+    /**
+     * The scopes being requested.
+     *
+     * @var array
+     */
+    protected $scopes = ['read'];
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getAuthUrl($state)
+    {
+        return $this->buildAuthUrlFromBase('https://'.$this->getSubdomain().'.zendesk.com/oauth/authorizations/new', $state);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getTokenUrl()
+    {
+        return 'https://'.$this->getSubdomain().'.zendesk.com/oauth/tokens';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getUserByToken($token)
+    {
+        $response = $this->getHttpClient()->get('https://'.$this->getSubdomain().'.zendesk.com/api/v2/users/me.json', [
+            'headers' => [
+                'Authorization' => 'Bearer '.$token,
+            ],
+        ]);
+
+        return json_decode($response->getBody(), true)['user'];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function mapUserToObject(array $user)
+    {
+        return (new User())->setRaw($user)->map([
+            'id'       => $user['id'],
+            'nickname' => null,
+            'name'     => $user['name'],
+            'email'    => $user['email'],
+            'avatar'   => null,
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getTokenFields($code)
+    {
+        return array_merge(parent::getTokenFields($code), ['grant_type' => 'authorization_code']);
+    }
+
+    /**
+     * Load the specified subdomain.
+     *
+     * @return string
+     */
+    private function getSubdomain()
+    {
+        return config('services.zendesk.subdomain');
+    }
+}
---
+++ src/ZendeskExtendSocialite.php
@@ -0,0 +1,12 @@
+<?php
+namespace SocialiteProviders\Zendesk;
+
+use SocialiteProviders\Manager\SocialiteWasCalled;
+
+class ZendeskExtendSocialite
+{
+    public function handle(SocialiteWasCalled $socialiteWasCalled)
+    {
+        $socialiteWasCalled->extendSocialite('zendesk', __NAMESPACE__.'\Provider');
+    }
+}
