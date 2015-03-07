15f5f24dff62701c5c2a6bafed47efee58540426
DraperStudio <hello@draperstud.io>
2015-03-07 12:02:03 +0100

    Update comments

--- src/ZendeskExtendSocialite.php
+++ src/ZendeskExtendSocialite.php
@@ -6,7 +6,9 @@ use SocialiteProviders\Manager\SocialiteWasCalled;
 class ZendeskExtendSocialite
 {
     /**
-     * Execute the provider.
+     * Register the provider.
+     *
+     * @param \SocialiteProviders\Manager\SocialiteWasCalled $socialiteWasCalled
      */
     public function handle(SocialiteWasCalled $socialiteWasCalled)
     {
